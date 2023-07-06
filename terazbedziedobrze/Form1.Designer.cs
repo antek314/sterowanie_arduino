c8112ca8ee741d065c54cedf14567d82057998b7
antek314 <antoni.mielcarek@student.put.poznan.pl>
2023-07-06 10:38:20 +0200

    troche dziala

--- terazbedziedobrze/Form1.Designer.cs
+++ terazbedziedobrze/Form1.Designer.cs
@@ -28,11 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -74,19 +82,75 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(52, 139);
+            this.button2.Location = new System.Drawing.Point(41, 131);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(258, 148);
+            this.button2.Size = new System.Drawing.Size(188, 39);
             this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
+            this.button2.Text = "Lampka";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 326);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(160, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 277);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(304, 378);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(317, 62);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(413, 56);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(503, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
@@ -95,6 +159,7 @@
             this.Name = "Form1";
             this.Text = "Wybierz";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,6 +172,12 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- terazbedziedobrze/Form1.cs
+++ terazbedziedobrze/Form1.cs
@@ -18,6 +18,10 @@ namespace terazbedziedobrze
         const string D = "Antek jest glupi";
         bool zapal = false;
         SerialPort port;
+        SerialPort port1;
+        SerialPort port2;
+
+
         string[] array = new string[3] { "a", "b", "c" };
         public Form1()
         {
@@ -27,7 +31,7 @@ namespace terazbedziedobrze
         private void Form1_Load(object sender, EventArgs e)
         {
             label1.Text = V;
-            foreach(string el in array)
+            foreach (string el in array)
             {
                 comboBox1.Items.Add(el);
             }
@@ -35,13 +39,20 @@ namespace terazbedziedobrze
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (przycisk == true) { label1.Text = V;
+            if (przycisk == true)
+            {
+                label1.Text = V;
                 przycisk = false;
             }
-            else { label1.Text = D;
+            else
+            {
+                label1.Text = D;
                 przycisk = true;
             }
             Console.WriteLine(V + " i " + D);
+            //serialPort1.Open();
+            //serialPort1.Write("a");
+            //serialPort1.Close();
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -54,22 +65,67 @@ namespace terazbedziedobrze
             string sd = "9600";
             //Task task = new Task();
             //AOChannel myaOChannel;
-            if(zapal==false)
+            if (zapal == false)
             {
                 port = new SerialPort("COM5", int.Parse(sd), Parity.None, 8, StopBits.One);
                 port.Open();
-                port.Write("c");
+                port.Write("cc");
                 port.Close();
                 zapal = true;
             }
-            else if(zapal==true)
+            else if (zapal == true)
             {
                 port = new SerialPort("COM5", int.Parse(sd), Parity.None, 8, StopBits.One);
                 port.Open();
-                port.Write("b");
+                port.Write("bb");
                 port.Close();
                 zapal = false;
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (progressBar1.Value <= 95)
+            {
+                progressBar1.Value += 5;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            string sd = "9600";
+            port = new SerialPort("COM5", int.Parse(sd), Parity.None, 8, StopBits.One);
+            port.Open();
+            int op;
+            op = port.ReadByte();
+            port.Close();
+            label3.Text = op.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string sd = "9600";
+            port1 = new SerialPort("COM5", int.Parse(sd), Parity.None, 8, StopBits.One);
+            port1.Open();
+            int op;
+            op = port1.ReadByte();
+            port1.Close();
+            label3.Text = op.ToString();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            string sd = "9600";
+            port2 = new SerialPort("COM5", int.Parse(sd), Parity.None, 8, StopBits.One);
+            port2.Open();
+            int op;
+            op = trackBar1.Value;
+            char d;
+            d = (char)op;
+            //string fal = op.ToString();
+            port2.Write(d.ToString());
+            port2.Close();
+            label4.Text = trackBar1.Value.ToString();
         }
     }
 }
